106a2131e93c36b48dcc20f974b2d94fef593f65
Jon Balon <development@jonbalon.com>
2016-06-21 12:17:10 -0400

    Removed jQuery from gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -44,10 +44,6 @@ gulp.task("html", function() {
 // Move Components.
 gulp.task("components", function() {
 
-  // jQuery.
-  gulp.src(modulesPath + "jquery/dist/jquery.js")
-    .pipe(gulp.dest(destPath + "components/jquery/"));
-
   // React.
   gulp.src(modulesPath + "react/dist/react.js")
     .pipe(gulp.dest(destPath + "components/react/"));
